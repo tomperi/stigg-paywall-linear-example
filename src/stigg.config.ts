cb559ab96b3d877f6c0617f9eab05610b5c8ca5c
Tom Peri <tomp228@gmail.com>
2023-08-01 16:08:32 +0300

    chore: add subset type

--- src/stigg.config.ts
+++ src/stigg.config.ts
@@ -1,8 +1,18 @@
-import { PaywallLocalization } from "@stigg/react-sdk";
+import { PaywallLocalization, PaywallPlan } from "@stigg/react-sdk";
 
-export const TEXT_OVERRIDES: Partial<PaywallLocalization> = {
+type Subset<K> = {
+  [attr in keyof K]?: K[attr] extends object
+      ? Subset<K[attr]>
+      : K[attr] extends object | null
+      ? Subset<K[attr]> | null
+      : K[attr] extends object | null | undefined
+      ? Subset<K[attr]> | null | undefined
+      : K[attr];
+};
+
+export const TEXT_OVERRIDES: Subset<PaywallLocalization> = {
   planCTAButton: {
-    startTrial: (plan) => `Get started with ${plan.displayName}`
+    startTrial: (plan: PaywallPlan) => `Get started with ${plan.displayName}`
   },
   highlightChip: "",
   price: {
